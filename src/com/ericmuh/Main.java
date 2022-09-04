c4566f97d8e81b07671864f141228d338c5df4e8
ericmuh <muhwezi1000@gmail.com>
2022-09-04 21:29:59 +0300

    update

--- src/com/ericmuh/Main.java
+++ src/com/ericmuh/Main.java
@@ -1,11 +1,35 @@
 package com.ericmuh;
 
-public class Main {
+public class Main { // This is a class named Main, that is public
 
-    public static void main(String[] args) {
-        // write your code here
+    public static void main(String[] args) { 
+        /* This is a function that returns void(nothing), that is also static
+         it also takes in a list of strings as its arguments
+         write your code here
+
+         Static ==> the variable belongs to a class ..
+         which means that we dont have to create a new instance of the Class to call the method/variable.
+         ==== CREATING a intance ====
+        SomeClass obj = new SomeClass()
+        obj.someMethod()
+        */
         System.out.println("Hello World");
 
+
+        // DATA Types {PRIMITIVE}
+
+        /*
+                Tpype   || Bytes || Range
+            ==========================
+                Byte    || 1     ||    [-128, 127]
+                short   || 2     ||    [-32K, 32K]
+                int     || 4      ||     [-2B, 2B]
+                long    || 8     ||
+                float   || 4     ||
+                double  || 8     ||
+                char    || 2     ||A, B, C, …
+                boolean || 1     ||true / false
+        */
 
     }
 }
