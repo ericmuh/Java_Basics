1f801789cd4997771cca7f0a83ea2bbaa29e037b
ericmuh <muhwezi1000@gmail.com>
2022-09-05 12:35:25 +0300

    getting started with Java language

---
+++ src/App.java
@@ -0,0 +1,21 @@
+public class App { // This is a class named Main, that is public
+
+    public static void main(String[] args) throws Exception {
+       
+        System.out.println("Hello World");
+
+    
+        // DECLARING VAIRIABLES
+        Byte someByte = 40;
+        System.out.println(someByte);
+        int someInt = 50;
+        System.out.println(someInt);
+        float someFloat = 50.5F;
+        System.out.println(someFloat);
+        char someChar = 'Q';
+        System.out.println(someChar);
+        boolean someBol = true;
+        System.out.println(someBol);
+
+    }
+}
